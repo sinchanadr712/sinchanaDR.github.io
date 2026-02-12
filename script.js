96e7c3de170a3827464aed045e387ab6f5759628
sinchanadr712 <119512623+sinchanadr712@users.noreply.github.com>
2026-02-12 17:03:39 +0530

    Add functions for downloading resume and sending messages

---
+++ script.js
@@ -0,0 +1,9 @@
+function downloadResume() {
+    alert("Resume download will be added soon!");
+}
+
+function sendMessage(event) {
+    event.preventDefault();
+
+    alert("Message sent successfully!");
+}
